44ccb2ae90d170ed97a3e15005a00b610f51f1fb
coliss86 <coliss86@yahoo.fr>
2015-12-31 00:21:43 +0100

    configuration de fancybox

--- static/js/webcam.js
+++ static/js/webcam.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
   $(".fancybox").fancybox({
     helpers: {
-      title : {
-        type : 'outside'
-      },
-      overlay : {
-        speedOut : 0
-      }
-    }
+      title: {
+            type: 'inside',
+            position: 'bottom'
+        }
+    },
+    afterLoad: function() {
+        this.title = '<a href="' + this.href + '">Download</a> ' + this.title;
+    },
+    afterShow: function() {
+        $('<div class="expander"></div>').appendTo(this.inner).click(function() {
+            $(document).toggleFullScreen();
+        });
+    },
+    loop: false,
+    nextEffect: 'none',
+    prevEffect: 'none',
+    openEffect: 'elastic',
+    closeEffect: 'elastic'
   });
 });
