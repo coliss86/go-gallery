94a80b60af34f7eecab68576ff4517bd951b8fb3
coliss86 <coliss86@yahoo.fr>
2016-01-01 22:56:33 +0100

    ajout de la catégorie pour les mois vides

--- src/webcam/ui.go
+++ src/webcam/ui.go
@@ -28,7 +28,7 @@ type Data struct {
 	Folder     string
 }
 
-var monthsName = map[string]string{"01": "Janvier", "02": "Février", "03": "Mars", "04": "Avril", "05": "Mai", "06": "Juin", "07": "Juillet", "08": "Aout", "09": "Septembre", "10": "Octobre", "11": "Novembre", "12": "Décembre"}
+var monthsName = map[string]string{"01": "Janvier", "02": "Février", "03": "Mars", "04": "Avril", "05": "Mai", "06": "Juin", "07": "Juillet", "08": "Aout", "09": "Septembre", "10": "Octobre", "11": "Novembre", "12": "Décembre", "": "Dossier"}
 
 var folderRE = regexp.MustCompile("([0-9]+)-([0-9]+).*")
 var ignoreRE = regexp.MustCompile(`.git|.svn|.DS_Store|Thumbs.db|meta.properties`)
