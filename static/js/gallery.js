95201d392391bd4298f8465c2a3210bfd511acbf
coliss86 <coliss86@yahoo.fr>
2016-06-05 22:16:14 +0200

    Ajout lien de récupération de l'image en direct. Fix #8

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -14,11 +14,11 @@ $(document).ready(function() {
         if (this.title == '') {
             this.title = this.href.substring(this.href.lastIndexOf("/") +1);
         }
-        this.title = '<a href="' + this.href.replace("/img/", "/download/") + '"><img src="/static/img/arrow_down_32.png" ></a><span title="' + this.title + '"> ' + this.title + '</span><span class="tag"><input type="text" name="tags" placeholder="Dossier" class="tm-input tm-input-small"/></span>';
+        this.title = '<a href="' + this.href.replace("/img/", "/download/") + '"><img src="/static/img/arrow_down_32.png" /></a>&nbsp;<a href="' + this.href + '"><img src="/static/img/link.png" /></a>&nbsp;<span title="' + this.title + '"> ' + this.title + '</span><span class="tag"><input type="text" name="tags" placeholder="Dossier" class="tm-input tm-input-small"/></span>';
     },
     afterShow: function() {
       var img = this.href.replace("/img/", "");
-      
+
       var tab = $(".tm-input").tagsManager({
         prefilled: folders,
         CapitalizeFirstLetter: true,
