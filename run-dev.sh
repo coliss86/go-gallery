dfc059b45360bf4e31c59dbed74b83c616398b9b
coliss86 <coliss86@yahoo.fr>
2016-06-27 00:09:38 +0200

    Valorisation automatique de la variable $GOPATH

--- run-dev.sh
+++ run-dev.sh
@@ -2,8 +2,16 @@
 
 if [ -z $GOPATH ];
 then
-  echo "Variable \$GOPATH is not set." >&2
-  exit 1
+  echo "Variable \$GOPATH is not set."
+  DIR=$(dirname \"$(readlink -f \"$0\")\")
+  GOPATH=$(readlink -f $DIR/../../../../)
+  export GOPATH
+  echo "Setting it to '$GOPATH'"
+  if [ ! -e "$GOPATH/src" ];
+  then
+    echo "Can't determine automatically the \$GOPATH value" >&2
+    exit 1
+  fi
 fi
 
 godep go build && ./go-gallery conf/gallery.conf
